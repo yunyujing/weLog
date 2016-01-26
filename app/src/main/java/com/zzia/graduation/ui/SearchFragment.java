5a3e8e446a0123c7b3c7ebc36847bf71157e3d14
yunyujing <15136271194@163.com>
2016-01-26 15:12:42 +0800

    配置本地的gradle

--- app/src/main/java/com/zzia/graduation/ui/SearchFragment.java
+++ app/src/main/java/com/zzia/graduation/ui/SearchFragment.java
@@ -3,9 +3,13 @@ package com.zzia.graduation.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.zzia.graduation.welog.R;
 
@@ -13,7 +17,9 @@ import com.zzia.graduation.welog.R;
  * Author: yunyujing
  * Date: 2015/12/1
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener{
+    private ImageView delete;
+    private EditText searchText;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,10 +28,40 @@ public class SearchFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        initView();
+        initView(view);
     }
 
-    private void initView() {
+    private void initView(View view) {
+        searchText= (EditText) view.findViewById(R.id.search_layout_edit);
+        delete= (ImageView) view.findViewById(R.id.search_edt_clean_img);
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==0){
+                    delete.setVisibility(View.GONE);
+                }else {
+                    delete.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+           case R.id.search_edt_clean_img:
+               searchText.setText("");
+               break;
+        }
     }
 }
